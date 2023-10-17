dd498956d2ac446cf8e6bccba1274a442e6c0b31
taijulsir <princetaijul123@gmail.com>
2023-10-17 21:46:05 +0600

    crate a navbar with navlinks

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter,RouterProvider,} from "react-router-dom";
 import Root from './Layout/Root';
+import Login from './Components/Login';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    children:[
+      {
+        path: '/login',
+        element: <Login></Login>
+      }
+    ]
   },
 ]);
 
